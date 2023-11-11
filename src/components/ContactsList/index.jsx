5aa5b79c9502866036244421dcf4b2eb68bc6880
Viktoriia <pecourses>
2023-11-11 17:00:42 +0200

    Add read contacts list

--- src/components/ContactsList/index.jsx
+++ src/components/ContactsList/index.jsx
@@ -1,5 +1,15 @@
-function ContactsList () {
-  return <div>ContactsList</div>
+import { connect } from 'react-redux'
+
+function ContactsList ({ contacts }) {
+  return (
+    <ul>
+      {contacts.map(c => (
+        <li key={c.id}>{JSON.stringify(c)}</li>
+      ))}
+    </ul>
+  )
 }
 
-export default ContactsList
+const mapStateToProps = ({ contactsList }) => contactsList
+
+export default connect(mapStateToProps)(ContactsList)
