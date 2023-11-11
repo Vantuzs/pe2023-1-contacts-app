2b7f4485f1b6f9085075fc67451f14d98d79a8d8
Viktoriia <pecourses>
2023-11-11 16:47:21 +0200

    Add base components structure

---
+++ src/pages/ContactsPage/index.jsx
@@ -0,0 +1,14 @@
+import ContactForm from '../../components/ContactForm'
+import ContactsList from '../../components/ContactsList'
+
+function ContactsPage () {
+  return (
+    <main>
+      <h2>Contacts</h2>
+      <ContactForm />
+      <ContactsList />
+    </main>
+  )
+}
+
+export default ContactsPage
