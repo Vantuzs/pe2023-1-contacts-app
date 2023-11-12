18d0b277a1c4afa7e2d58957edd67a21c57eff33
Viktoriia <pecourses>
2023-11-12 16:52:30 +0200

    Add posts base

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-// App
-//   ContactsPage
-//     ContactForm
-//     ContactsList
-//       ContactsListItem
-//       ...
-//       ContactsListItem
-
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ContactsPage from './pages/ContactsPage';
+import PostsPage from './pages/PostsPage';
 
 function App () {
-  return <ContactsPage />;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/contacts'>Contacts</Link>
+          </li>
+          <li>
+            <Link to='/posts'>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/contacts'>
+          <ContactsPage />
+        </Route>
+        <Route path='/posts'>
+          <PostsPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/PostsPage/index.jsx
@@ -0,0 +1,12 @@
+import PostsList from '../../components/PostsList';
+
+function PostsPage () {
+  return (
+    <main>
+      <h2>Posts</h2>
+      <PostsList />
+    </main>
+  );
+}
+
+export default PostsPage;
