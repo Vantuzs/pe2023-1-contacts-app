1c2533c50bafe5b76bae8f2486242e286884a255
Viktoriia <pecourses>
2023-11-12 16:11:55 +0200

    Add axios examples

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://randomuser.me/api',
+});
+
+axiosInstance
+  .get('/?results=2')
+  .then(({ data }) => console.log(data))
+  .catch(err => console.log(err));
+
+axiosInstance
+  .get('/?gender=female')
+  .then(({ data }) => console.log(data))
+  .catch(err => console.log(err));
+
+// axios
+//   .get('https://randomuser.me/api/?results=2')
+//   .then(({ data }) => console.log(data))
+//   .catch(err => console.log(err));
+
+// async function loadUsers () {
+//   try {
+//     const { data } = await axios.get('https://randomuser.me/api/?results=2');
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// loadUsers();
