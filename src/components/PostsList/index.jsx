3e790dfe626e7542d6492fb61459db74309f1dbe
Viktoriia <pecourses>
2023-11-12 17:27:22 +0200

    Add posts list with state

--- src/components/PostsList/index.jsx
+++ src/components/PostsList/index.jsx
@@ -1,5 +1,22 @@
-function PostsList () {
-  return <div>PostsList</div>;
+import { connect } from 'react-redux';
+
+function PostsList ({ posts, isFetching, error }) {
+  const mapPosts = p => (
+    <li key={p.id}>
+      <h3>{p.title}</h3>
+      <p>{p.body}</p>
+    </li>
+  );
+
+  return (
+    <>
+      {isFetching && <div>Loading...</div>}
+      {error && <div>!!!ERROR!!!</div>}
+      {!isFetching && !error && <ul>{posts.map(mapPosts)}</ul>}
+    </>
+  );
 }
 
-export default PostsList;
+const mapStateToProps = ({ postsList }) => postsList;
+
+export default connect(mapStateToProps)(PostsList);
